556279241092fad04fb991f59c1829501a969da5
Bria Fincher <briafincher@Brias-MacBook-Pro.local>
2020-11-23 18:51:38 -0800

    WIP

--- database/migrations/2020_11_24_022559_create_users_table.php
+++ database/migrations/2020_11_24_022559_create_users_table.php
@@ -12,14 +12,23 @@ class CreateUsersTable extends Migration
      * @return void
      */
     public function up()
+    // $table->id();
+    //             $table->string('name');
+    //             $table->string('email')->unique();
+    //             $table->timestamp('email_verified_at')->nullable();
+    //             $table->string('password');
+    //             $table->rememberToken();
+    //             $table->timestamps();
+
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            // Address;
+            $table->string('phone');
+            // PaymentMethod;
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,117 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// TODO: Should any routes be moved to api.php?
 Route::get('/', function () {
+    return view('main');
+});
+
+Route::post('/magic', function (Request $request) {
+	// Request
+	// $request = {
+	// 	"firstName": "string",
+	// 	"lastName": "string",
+	// 	"email": "string", # unique
+	// 	"address": {
+	// 		"street1": "string",
+	// 		"street2": "string",
+	// 		"city": "string",
+	// 		"state": "string",
+	// 		"zip": "string"
+	// 	},
+	// 	"phone": "string",
+	// 	"quantity": number,
+	// 	"total": "string",
+	// 	"payment": {
+	// 		"ccNum": "string",
+	// 		"exp": "string" 
+	// 	}
+	// }
+
+	$params = [
+		'first_name' => $request->firstName,
+		'last_name' => $request->lastName,
+		'email' => $request->email,
+		'address' => new Address(),
+		'phone' => $request->phone,
+		'quantity' => $request->quantity,
+		'total' => $request->quantity * 49.99,
+		'payment' => new PaymentMethod()
+	];
+
+	$user = User::find($email);
+	$orders = $user->orders_for_month(date('M'));
+
+	if (count($orders) < 3) {
+		$new_order = new Order($params);
+	}
+
+	// Response
+	// return
+	// 201 CREATED
+	// {
+	// 	"id": uid
+	// }
+
+    return view('main');
+});
+
+Route::get('/magic/{uid})', function ($uid) {
+	// Response
+	// if ($success) {
+	// 	return 
+	// 	{
+	// 		"firstName": "string", 
+	// 		"lastName": "string", 
+	// 		"email": "string", 
+	// 		"address": {
+	// 			"street1": "string", 
+	// 			"street2": "string", 
+	// 			"city": "string", 
+	// 			"state": "string", 
+	// 			"zip": "string",
+	// 		},
+	// 		"phone": "string", 
+	// 		"payment": {
+	// 			"ccNum": "string",
+	// 			"exp": "string", 
+	// 		},
+	// 		"quantity": number, 
+	// 		"total": "string",
+	// 		 "orderDate": date, 
+	// 		 "fulfilled": bool,
+	// 	}
+	// } else {
+	// 	404 "resource not found"
+	// }
+
+    return view('main');
+});
+
+Route::patch('/magic', function (Request $request) {
+	// Request
+	// $request = {
+	// 	"id": uid, 
+	// 	"fulfilled": bool
+	// }
+
+	// Response
+	// if ($success) {
+	// 	return 200 || 204 "resource updated successfully"
+	// } else {
+	// 	return 404 "resource not found"
+	// }
+
+    return view('main');
+});
+
+Route::delete('/magic/{uid}', function ($uid) {
+	// Response
+	// if ($success) {
+	// 	return 200 || 204 "resource deleted successfully"
+	// } else {
+	// 	return 404 "resource not found"
+	// }
+
     return view('main');
 });
